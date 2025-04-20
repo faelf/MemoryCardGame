2742ed2e11ffaf6bfd93363808de12add1639367
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-04-20 15:59:23 +0100

    Add alt attribute to img

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,18 +15,30 @@ let gameStats = {
 
 // Storing the cards
 const cardFiles = [
-  { id: 1,  imgSrc: "assets/images/zelda1.webp", name: "card1" },
-  { id: 2,  imgSrc: "assets/images/zelda2.webp", name: "card2" },
-  { id: 3,  imgSrc: "assets/images/zelda3.webp", name: "card3" },
-  { id: 4,  imgSrc: "assets/images/zelda4.webp", name: "card4" },
-  { id: 5,  imgSrc: "assets/images/zelda5.webp", name: "card5" },
-  { id: 6,  imgSrc: "assets/images/zelda6.webp", name: "card6" },
-  { id: 7,  imgSrc: "assets/images/zelda1.webp", name: "card1" },
-  { id: 8,  imgSrc: "assets/images/zelda2.webp", name: "card2" },
-  { id: 9,  imgSrc: "assets/images/zelda3.webp", name: "card3" },
-  { id: 10, imgSrc: "assets/images/zelda4.webp", name: "card4" },
-  { id: 11, imgSrc: "assets/images/zelda5.webp", name: "card5" },
-  { id: 12, imgSrc: "assets/images/zelda6.webp", name: "card6" }
+  { alt: "Zelda Echoes of Wisdom", id: 1,
+    imgSrc: "assets/images/zelda1.webp", name: "card1" },
+  { alt: "Zelda Twilight Princess", id: 2,
+    imgSrc: "assets/images/zelda2.webp", name: "card2" },
+  { alt: "Zelda a Link to the Past", id: 3,
+    imgSrc: "assets/images/zelda3.webp", name: "card3" },
+  { alt: "Zelda Link's Awakening", id: 4,
+    imgSrc: "assets/images/zelda4.webp", name: "card4" },
+  { alt: "Zelda Majora's Mask", id: 5,
+    imgSrc: "assets/images/zelda5.webp", name: "card5" },
+  { alt: "Zelda the Wind Waker", id: 6,
+    imgSrc: "assets/images/zelda6.webp", name: "card6" },
+  { alt: "Zelda Echoes of Wisdom", id: 7,
+    imgSrc: "assets/images/zelda1.webp", name: "card1" },
+  { alt: "Zelda Twilight Princess", id: 8,
+    imgSrc: "assets/images/zelda2.webp", name: "card2" },
+  { alt: "Zelda a Link to the Past", id: 9,
+    imgSrc: "assets/images/zelda3.webp", name: "card3" },
+  { alt: "Zelda Link's Awakening", id: 10,
+    imgSrc: "assets/images/zelda4.webp", name: "card4" },
+  { alt: "Zelda Majora's Mask", id: 11,
+    imgSrc: "assets/images/zelda5.webp", name: "card5" },
+  { alt: "Zelda the Wind Waker", id: 12,
+    imgSrc: "assets/images/zelda6.webp", name: "card6" }
 ];
 
 /*
@@ -53,6 +65,7 @@ function generateCards () {
     cardFrontImg.src = cards[i].imgSrc;
     // Add the name of the card
     card.setAttribute("data-name", cards[i].name);
+    cardFrontImg.setAttribute("alt", cards[i].alt);
     // Attach the cards to Game Board
     gameBoard.appendChild(card);
     card.appendChild(cardFrontImg);
