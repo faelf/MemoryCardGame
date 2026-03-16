c081bd64cda84c9368c14e4b9104096f4f4a4e0c
Rafael Ferreira <rafaelf_s@hotmail.com>
2026-03-16 21:32:45 +0000

    feat: move card storage to a separate file

---
+++ assets/js/cards.js
@@ -0,0 +1,15 @@
+// Storing the cards
+export const cardFiles = [
+  { alt: "Zelda", id: 1, imgSrc: "assets/images/zelda1.webp", name: "card1" },
+  { alt: "Zelda", id: 2, imgSrc: "assets/images/zelda2.webp", name: "card2" },
+  { alt: "Zelda", id: 3, imgSrc: "assets/images/zelda3.webp", name: "card3" },
+  { alt: "Zelda", id: 4, imgSrc: "assets/images/zelda4.webp", name: "card4" },
+  { alt: "Zelda", id: 5, imgSrc: "assets/images/zelda5.webp", name: "card5" },
+  { alt: "Zelda", id: 6, imgSrc: "assets/images/zelda6.webp", name: "card6" },
+  { alt: "Zelda", id: 7, imgSrc: "assets/images/zelda1.webp", name: "card1" },
+  { alt: "Zelda", id: 8, imgSrc: "assets/images/zelda2.webp", name: "card2" },
+  { alt: "Zelda", id: 9, imgSrc: "assets/images/zelda3.webp", name: "card3" },
+  { alt: "Zelda", id: 10, imgSrc: "assets/images/zelda4.webp", name: "card4" },
+  { alt: "Zelda", id: 11, imgSrc: "assets/images/zelda5.webp", name: "card5" },
+  { alt: "Zelda", id: 12, imgSrc: "assets/images/zelda6.webp", name: "card6" },
+];
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,5 @@
-import { scores } from "../js/scores.js";
+import { scores } from "./scores.js";
+import { cardFiles } from "./cards.js";
 
 // Getting HTML info
 const gameBoard = document.getElementById("game-board");
@@ -14,22 +15,6 @@ let gameStats = {
   points: 0,
   turns: 0,
 };
-
-// Storing the cards
-const cardFiles = [
-  { alt: "Zelda", id: 1, imgSrc: "assets/images/zelda1.webp", name: "card1" },
-  { alt: "Zelda", id: 2, imgSrc: "assets/images/zelda2.webp", name: "card2" },
-  { alt: "Zelda", id: 3, imgSrc: "assets/images/zelda3.webp", name: "card3" },
-  { alt: "Zelda", id: 4, imgSrc: "assets/images/zelda4.webp", name: "card4" },
-  { alt: "Zelda", id: 5, imgSrc: "assets/images/zelda5.webp", name: "card5" },
-  { alt: "Zelda", id: 6, imgSrc: "assets/images/zelda6.webp", name: "card6" },
-  { alt: "Zelda", id: 7, imgSrc: "assets/images/zelda1.webp", name: "card1" },
-  { alt: "Zelda", id: 8, imgSrc: "assets/images/zelda2.webp", name: "card2" },
-  { alt: "Zelda", id: 9, imgSrc: "assets/images/zelda3.webp", name: "card3" },
-  { alt: "Zelda", id: 10, imgSrc: "assets/images/zelda4.webp", name: "card4" },
-  { alt: "Zelda", id: 11, imgSrc: "assets/images/zelda5.webp", name: "card5" },
-  { alt: "Zelda", id: 12, imgSrc: "assets/images/zelda6.webp", name: "card6" },
-];
 
 /*
   Shuffle the cards
